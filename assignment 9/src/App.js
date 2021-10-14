c59b3f2d65addc606834351a012a3abb5207a71a
Manik-Rangar <87720956+Manik-Rangar@users.noreply.github.com>
2021-10-14 23:02:29 +0530

    Add files via upload

---
+++ assignment 9/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TodoApp from './components/TodoApp'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <TodoApp/>
+        <div id="myName"> &#169; Manik-1911981088</div>
+    </div>
+  )
+}
+
+export default App
+
